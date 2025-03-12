a02da3abac8e0e6946f266a28a726f06178ce320
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-03-12 13:05:41 +0000

    Implement GET /report in controller

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dsmeta.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
@@ -24,14 +24,17 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleMinDTO>> getReport(
+		@RequestParam(name="minDate", required = false) String minDate, @RequestParam(name= "maxDate",required = false) String maxDate,
+		@RequestParam(required = false) String name, Pageable pageable) {
+
+			Page<SaleMinDTO> result = service.searchAll(minDate,maxDate,name, pageable);
+			return ResponseEntity.ok(result);
+
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
+	public ResponseEntity<?> getSummary(){
 		return null;
 	}
 }
