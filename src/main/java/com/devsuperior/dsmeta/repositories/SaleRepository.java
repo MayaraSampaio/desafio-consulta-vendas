fd333f1cc95e94121f0b751c5466432ff90f4e48
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-03-12 13:00:58 +0000

    Implement query searchAll for Sales Report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.dsmeta.repositories;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+import java.time.LocalDate;
+
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+
+    @Query(value = "SELECT s FROM Sale s " +
+            "JOIN FETCH s.seller seller " +
+            "WHERE s.date BETWEEN :minDate AND :maxDate " +
+            "AND UPPER(seller.name) LIKE UPPER(CONCAT('%', :nameSeller, '%'))",
+            countQuery = "SELECT COUNT(s) FROM Sale s " +
+                    "JOIN s.seller seller " +
+                    "WHERE s.date BETWEEN :minDate AND :maxDate " +
+                    "AND UPPER(seller.name) LIKE UPPER(CONCAT('%', :nameSeller, '%'))")
+    Page<Sale> searchAll(LocalDate minDate, LocalDate maxDate, String nameSeller, Pageable pageable);
 
 }
